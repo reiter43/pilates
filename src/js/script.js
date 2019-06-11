228cf38c504274b97d15faf7d565d41b37be5e33
reiter43 <43reiter@gmail.com>
2019-06-11 11:57:47 +0300

    Заанимировал кнопку-гамбургер и создал скрытое меню

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-          items:1,
-          loop:true,
-          dots:true,
-          smartSpeed:1500,
-          nav:true,
-          navText: [
-              "<img src=\"img/images/Стрелка_левая.png\">",
-              "<img src=\"img/images/Стрелка_правая.png\">",              
-          ],
-          
-      });
-  });
+        items: 1,
+        loop: true,
+        dots: true,
+        smartSpeed: 1500,
+        nav: true,
+        navText: [
+            "<img src=\"img/images/Стрелка_левая.png\">",
+            "<img src=\"img/images/Стрелка_правая.png\">",
+        ],
+
+    });
+
+    $(".burger").on('click', function (e) {
+        e.preventDefault;
+        $(this).toggleClass("burger--active");
+        $(".nav").toggleClass("visible");
+    });
+});
+
